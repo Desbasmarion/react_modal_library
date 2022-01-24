7f029a31f1df1af1aa7b32c6d1e9f560c697cfc7
Marion Desbas <desbasmarion@yahoo.fr>
2022-01-24 15:07:00 +0100

    Creation modal library 🎉

---
+++ src/lib/components/Modal.js
@@ -0,0 +1,32 @@
+import './styles/modal.scss'
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Modal = ({ props }) => {
+
+  Modal.propTypes = {
+		props: PropTypes.string,
+	};
+
+  const [ isOpen, setIsOpen ] = useState(true)
+
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <Fragment>
+      {isOpen && <div className="modal-container">
+            <div className="overlay modal-trigger"></div>
+            <div className="modal" role="dialog" aria-labelledby="modalTitle" aria-describedby="dialogDesc">
+                <button 
+                aria-label="close modal"
+                className="close-modal modal-trigger" onClick={closeModal}>X</button>
+                <p id="dialogDesc">{props}</p>
+            </div>
+        </div>}
+    </Fragment>
+  );
+};
+
+export default Modal;
